5b06df872152d0e6c915ccd0bb73883a56c05064
Raleigh Martin <raleighmartin@pm.me>
2023-11-12 13:45:37 -0500

    added more settings

--- src/settings.rs
+++ src/settings.rs
@@ -10,6 +10,7 @@ pub enum GoProSetting {
   Resolution(Hero11Resolution),
   Fps(Hero11FPS),
   AutoPowerDown(Hero11AutoPowerDown),
+  VideoDigitalLense(Hero11VideoDigitalLense),
 }
 
 pub enum Hero11Resolution {
@@ -128,3 +129,179 @@ where
     [0x01, 0x3B, 0x00]
   }
 }
+
+enum Hero11VideoDigitalLense {
+  Wide,
+  Superview,
+  Linear,
+  MaxSuperview,
+  LinearHorizonLeveling,
+  Hyperview,
+  LinearHorizonLock
+}
+
+impl AsRef<Hero11VideoDigitalLense> for Hero11VideoDigitalLense {
+  fn as_ref(&self) -> &Hero11VideoDigitalLense {
+    self
+  }
+}
+
+use Hero11VideoDigitalLense as H11VDL;
+impl<T> Sendable for T 
+where 
+  T: AsRef<H11DL>
+{
+  fn as_bytes(&self) -> &'static [u8] {
+    match self.as_ref() {
+      H11DL::Wide => &[0x03, 0x79, 0x01, 0x00],
+      H11DL::Superview => &[0x03, 0x79, 0x01, 0x03],
+      H11DL::Linear => &[0x03, 0x79, 0x01, 0x04],
+      H11DL::MaxSuperview => &[0x03, 0x79, 0x01, 0x07],
+      H11DL::LinearHorizonLeveling => &[0x03, 0x79, 0x01, 0x08],
+      H11DL::Hyperview => &[0x03, 0x79, 0x01, 0x09],
+      H11DL::LinearHorizonLock => &[0x03, 0x79, 0x01, 0x0A],
+    }
+  }
+
+  fn response_value_bytes(&self) -> &'static [u8] {
+    [0x02, 0x79, 0x00]
+  }
+}
+
+
+enum Hero11PhotoDigitalLense {
+  MaxSuperview,
+  Wide,
+  Linear
+}
+
+impl AsRef<Hero11PhotoDigitalLense> for Hero11PhotoDigitalLense {
+  fn as_ref(&self) -> &Hero11PhotoDigitalLense {
+    self
+  }
+}
+
+use Hero11PhotoDigitalLense as H11PDL;
+impl<T> Sendable for T 
+where 
+  T: AsRef<H11PDL>
+{
+  fn as_bytes(&self) -> &'static [u8] {
+    match self.as_ref() {
+      H11PDL::MaxSuperview => &[0x03, 0x7A, 0x01, 0x64],
+      H11PDL::Wide => &[0x03, 0x7A, 0x01, 0x65],
+      H11PDL::Linear => &[0x03, 0x7A, 0x01, 0x66],
+    }
+  }
+
+  fn response_value_bytes(&self) -> &'static [u8] {
+    [0x02, 0x7A, 0x00]
+  }
+}
+
+enum Hero11TimeLapseDigitalLense {
+  MaxSuperview,
+  Wide,
+  Linear
+}
+
+impl AsRef<Hero11TimeLapseDigitalLense> for Hero11TimeLapseDigitalLense {
+  fn as_ref(&self) -> &Hero11TimeLapseDigitalLense {
+    self
+  }
+}
+
+use Hero11TimeLapseDigitalLense as H11TDL;
+impl<T> Sendable for T 
+where 
+  T: AsRef<H11TDL>
+{
+  fn as_bytes(&self) -> &'static [u8] {
+    match self.as_ref() {
+      H11TDL::MaxSuperview => &[0x03, 0x7B, 0x01, 0x64],
+      H11TDL::Wide => &[0x03, 0x7B, 0x01, 0x65],
+      H11TDL::Linear => &[0x03, 0x7B, 0x01, 0x66],
+    }
+  }
+
+  fn response_value_bytes(&self) -> &'static [u8] {
+    [0x02, 0x7B, 0x00]
+  }
+}
+
+enum Hero11MediaFormat {
+  TimeLapseVideo,
+  TimeLapsePhoto,
+  NightLapsePhoto,
+  NightLapseVideo,
+}
+
+impl AsRef<Hero11MediaFormat> for Hero11MediaFormat {
+  fn as_ref(&self) -> &Hero11MediaFormat {
+    self
+  }
+}
+
+use Hero11MediaFormat as H11MF;
+impl<T> Sendable for T 
+where 
+  T: AsRef<H11MF>
+{
+  fn as_bytes(&self) -> &'static [u8] {
+    match self.as_ref() {
+      H11MF::TimeLapseVideo => &[0x03, 0x80, 0x01, 0x0D],
+      H11MF::TimeLapsePhoto => &[0x03, 0x80, 0x01, 0x14],
+      H11MF::NightLapsePhoto => &[0x03, 0x80, 0x01, 0x15],
+      H11MF::NightLapseVideo => &[0x03, 0x80, 0x01, 0x1A],
+    }
+  }
+
+  fn response_value_bytes(&self) -> &'static [u8] {
+    [0x02, 0x80, 0x00]
+  }
+}
+
+enum AntiFlicker {
+  SixtyHertz,
+  FiftyHertz,
+}
+
+impl AsRef<AntiFlicker> for AntiFlicker {
+  fn as_ref(&self) -> &AntiFlicker {
+    self
+  }
+}
+
+use AntiFlicker as AF;
+impl<T> Sendable for T 
+where 
+  T: AsRef<AF>
+{
+  fn as_bytes(&self) -> &'static [u8] {
+    match self.as_ref() {
+      AF::SixtyHertz => &[0x03, 0x86, 0x01, 0x02],
+      AF::FiftyHertz => &[0x03, 0x86, 0x01, 0x03],
+    }
+  }
+
+  fn response_value_bytes(&self) -> &'static [u8] {
+    [0x02, 0x86, 0x00]
+  }
+}
+
+enum Hero11Hypersmooth {
+  Off,
+  Low,
+  Boost,
+  Auto,
+}
+
+impl AsRef<Hero11Hypersmooth> for Hero11Hypersmooth {
+  fn as_ref(&self) -> &Hero11Hypersmooth {
+    self
+  }
+}
+
+use Hero11Hypersmooth as H11HS;
+
+
